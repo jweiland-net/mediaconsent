c92bb40f12bdad0a0b6cb9ea0be8d83bd0d32425
Pascal Rinker <info@kronova.net>
2021-11-15 10:45:39 +0100

    [TASK] TYPO3 8.7 legacy release
    This release will not get all later new features from the master version!

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '0.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '8.7.0-8.7.99',
         ]
     ]
 ];

f3478f58829954a6dee5849d03560f19ec46ea67
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-01-31 10:33:13 +0100

    Icons moved to Configuration/Icon.php and added .editorconfig and .crowdin.yml

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'tx_mediaconsent_cns' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:mediaconsent/Resources/Public/Icons/Extension.svg',
+    ],
+];

749bf7b0de35dfbdd7a94bcb511aebab86225452
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-01-31 10:40:51 +0100

    Removec TSconfig inclusion from ext_localconf and added Configuration/page.tsconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,3 @@ declare(strict_types=1);
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mediaconsent/Configuration/PageTS/PageTSConfig.tsconfig">'
-);

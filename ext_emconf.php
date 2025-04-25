4e964fa2f3f50b7a1edf415de486056326e7095d
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-04-25 15:28:16 +0200

    [TASK] Configuration updates

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jweiland.net',
     'author_email' => 'projects@jweiland.net',
     'state' => 'stable',
-    'version' => '2.0.1',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.8-12.4.99',
+            'typo3' => '13.4.3-13.4.99',
         ]
     ]
 ];

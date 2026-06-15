0f5bcd43a7a13c5bc477c8a87ebaaacef381efef
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-06-15 09:36:05 +0200

    [TASK] Replace `$this->atLeastOnce()` with `self::atLeastOnce()` in unit tests for consistency

--- Tests/Unit/DataProcessing/MediaConsentProcessorTest.php
+++ Tests/Unit/DataProcessing/MediaConsentProcessorTest.php
@@ -23,7 +23,7 @@ class MediaConsentProcessorTest extends UnitTestCase
     public function processReturnsProcessedDataWithWrapperActiveWhenMediaConsentItemIsSet(): void
     {
         $cObj = $this->createMock(ContentObjectRenderer::class);
-        $cObj->expects($this->atLeastOnce())
+        $cObj->expects(self::atLeastOnce())
             ->method('getRequest')
             ->willReturn($this->createMock(ServerRequestInterface::class));
 
